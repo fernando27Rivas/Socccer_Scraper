877853fdb20bf5d5ee6cfc6cd3cb75707ee49599
fernando27Rivas <fernandorivasarevalo27@gmail.com>
2020-03-18 10:24:01 -0600

    Add scrapper to pdf

--- webScrapy/webScrapy/spiders/scraper.py
+++ webScrapy/webScrapy/spiders/scraper.py
@@ -13,8 +13,10 @@ from selenium.webdriver.support.ui import Select
 from .. import settings
 from selenium.common.exceptions import NoSuchElementException
 from pathlib import Path
+import pathlib
 import os
 import re
+from .. import  pdf_extraction
 from scrapy.http import Request
 
 
@@ -30,8 +32,8 @@ class ScraperSpider(CrawlSpider):
     search_url = 'https://nursefly.com'
     start_urls = ['https://nursefly.com']
      #sleep time
-    short_sleep=2
-    medium_sleep=4
+    short_sleep=4
+    medium_sleep=5
     long_sleep=10
 
 
@@ -46,6 +48,7 @@ class ScraperSpider(CrawlSpider):
         current_directory=str(Path(__file__).parent.parent.absolute())
         #adding pdf for windows chrome path change only to "/pdf/" on linux
         path_dir=current_directory+'/pdf/'
+
         #defining the path when the data csv will be stored
         csv_dir=current_directory+"/data/"
 
@@ -53,6 +56,7 @@ class ScraperSpider(CrawlSpider):
         #creating the path on the OS
         Path(path_dir).mkdir(parents=True,exist_ok=True)
         Path(csv_dir).mkdir(parents=True,exist_ok=True)
+
         
         #avoid prompt severeal files downloaded and selecting the custom path for downloads
         prefs = {"download.default_directory": path_dir,"profile.default_content_setting_values.automatic_downloads": 1}#,"profile.managed_default_content_settings.images": 2}
@@ -63,15 +67,22 @@ class ScraperSpider(CrawlSpider):
         #prefs = {"download.default_directory": path }
         chrome_options.add_experimental_option('prefs',prefs)
         #chrome_options.add_argument('--headless')
-        chrome_options.add_argument('window-size=1024x768')
+        #chrome_options.add_argument('window-size=1024x768')
         #chrome_options.add_argument("disable-gpu")
         #chrome_options.add_argument('--no-sandbox')
         #chrome_options.add_argument('--disable-dev-shm-usage')
-        # chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--start-maximized")
 
         browser = webdriver.Chrome(settings.chromedriver_path,chrome_options=chrome_options)
-        browser.get('chrome://settings/')
-        browser.execute_script('chrome.settingsPrivate.setDefaultZoom(0.8);')
+        #browser.get('chrome://settings/')
+        #browser.execute_script('chrome.settingsPrivate.setDefaultZoom(0.8);')
+        #time.sleep(self.medium_sleep)
+
+        if (self.file_exist(csv_path=csv_dir)):
+            comparator_id=self.reader(csv_path=csv_dir)
+        else:
+            comparator_id=None
+
 
         browser.get(url)
         time.sleep(self.short_sleep)
@@ -79,63 +90,46 @@ class ScraperSpider(CrawlSpider):
 
         # xpath to click sign in button
         try:
-            browser.find_element_by_xpath("//div[contains(@class, 'auth-section')]/button[2]").click()
+            browser.find_element_by_xpath("//div[contains(@class, 'auth-section')]/button[1]").click()
             time.sleep(self.short_sleep)
 
         except NoSuchElementException:
             pass
-            browser.find_element_by_xpath("//div[contains(@class, 'auth-section')]/button[2]").click()
+            browser.find_element_by_xpath("//div[contains(@class, 'auth-section')]/button[1]").click()
             time.sleep(self.short_sleep)
 
-        #xpath to click sing in div
-        try:
-            browser.find_element_by_xpath("//div[text()='Sign in']").click()
-            time.sleep(self.short_sleep)
-        except NoSuchElementException:
-            pass
-            browser.find_element_by_xpath("//div[text()='Sign in']").click()
-            time.sleep(self.short_sleep)
+
 
         #xpath to click login email
         try:
-            browser.find_element_by_xpath("//button[contains(@class,'QA__LoginWithEmail--Button sc-bdVaJa gTGaiA')]").click()
+            user=browser.find_element_by_id("phoneOrEmailInput")
+            user.send_keys(choice)
             time.sleep(self.short_sleep)
         except NoSuchElementException:
             pass
-            browser.find_element_by_xpath("//button[contains(@class,'QA__LoginWithEmail--Button sc-bdVaJa gTGaiA')]").click()
+            user = browser.find_element_by_id("phoneOrEmailInput")
+            user.send_keys(choice)
             time.sleep(self.short_sleep)
-
+         #
+        try:
+            browser.find_element_by_xpath("//*[@id='root']/div/main/div[1]/div/div/form/button").click()
+            time.sleep(self.short_sleep)
+        except NoSuchElementException:
+            pass
+            browser.find_element_by_xpath("//*[@id='root']/div/main/div[1]/div/div/form/button").click()
+            time.sleep(self.short_sleep)
         #fill login info    
         try:
-            user=browser.find_element_by_id('email')
+
+            pasw = browser.find_element_by_id("password")
+            pasw.send_keys(choice2)
             time.sleep(self.short_sleep)
         except NoSuchElementException:
             pass
-            user = browser.find_element_by_id('email')
+            pasw = browser.find_element_by_id("password")
+            pasw.send_keys(choice2)
             time.sleep(self.short_sleep)
 
-        try:
-            user.send_keys(choice)
-            time.sleep(self.short_sleep)
-        except NoSuchElementException:
-            pass
-            user.send_keys(choice)
-            time.sleep(self.short_sleep)
-        try:
-            pasw=browser.find_element_by_id('password')
-            time.sleep(self.short_sleep)
-        except NoSuchElementException:
-            pass
-            pasw = browser.find_element_by_id('password')
-            time.sleep(self.short_sleep)
-        try:
-            pasw.send_keys(choice2)
-            time.sleep(self.short_sleep)
-        except NoSuchElementException:
-            pass
-            pasw.send_keys(choice2)
-            time.sleep(self.short_sleep)
-        # submit to login
         try:
             browser.find_element_by_xpath("//button[@type='submit']").click()
         except NoSuchElementException:
@@ -168,32 +162,61 @@ class ScraperSpider(CrawlSpider):
         #current pdfs
         current_pdfs=self.get_list_pdfs_downloaded(path_dir)
 
+        element_ids=browser.find_element_by_xpath \
+            ("//div/table/tbody/tr[1]/td[1]").text
+
+        print("El ultimo ID es : " )
+        print(element_ids)
+
+        save_data=None
+        if(comparator_id is not None):
+            if (str(element_ids)== str(comparator_id)):
+                print("Es el mismo ID no hay nuevos resultados")
+                save_data=False
+            else:
+                print("Hay nuevos datos")
+                save_data=True
+        else:
+            print("No hay registro previo se guardara todos los pdf")
+            save_data=True
+
+
+
+
         while(i<=len(var_len)):
             print(f"\033[94m Message: {i} \033[0m") 
 
             nurse_name=self.get_actual_nurse(i,browser)
-
-            #if there's not name on pdf list it will be able to download a new pdf            
-            if(not nurse_name in current_pdfs):
-                try:
-                    time.sleep(self.medium_sleep)
-                    browser.find_element_by_xpath("//tr["+ str(i)+"]/td[6]/div/div/div[1]").click()
-                except NoSuchElementException:
-                    pass
-                    time.sleep(self.long_sleep)
-                    browser.find_element_by_xpath("//tr[" + str(i) + "]/td[6]/div/div/div[1]").click()
-                #wait for iterate another time until pdf get downloaded
-                time.sleep(3)
-                while self.has_any_download_active(path_dir):
-                    time.sleep(1)
+            element_id = browser.find_element_by_xpath \
+                ("//div/table/tbody/tr[1]/td[1]").text
+            #if there's not name on pdf list it will be able to download a new pdf
+            if( not(str(element_id)==str(comparator_id))):
+                if(not nurse_name in current_pdfs):
+                    try:
+                        time.sleep(self.medium_sleep)
+                        browser.find_element_by_xpath("//tr["+ str(i)+"]/td[6]/div/div/div[1]").click()
+                    except NoSuchElementException:
+                        pass
+                        time.sleep(self.long_sleep)
+                        browser.find_element_by_xpath("//tr[" + str(i) + "]/td[6]/div/div/div[1]").click()
+                    #wait for iterate another time until pdf get downloaded
+                    time.sleep(3)
+                    while self.has_any_download_active(path_dir):
+                        time.sleep(1)
             i=i+1
         #await for the last cv    
         time.sleep(self.long_sleep)
         self.clean_repeated_pdfs(path_dir)
 
         browser.close()
-        print(f"\033[94m SCRIPT PDF \033[0m")   
-        self.extractPdfDataToCsv(pdf_path=path_dir,csv_path=csv_dir)
+        print(f"\033[94m SCRIPT PDF \033[0m")
+
+
+        if (save_data):
+            self.write_csv(element_id=element_ids, csv_path=csv_dir)
+            pdf_extraction.extract_data()
+
+
 
     def get_actual_nurse(self,index,browser):
         actual_nurse=browser.find_element_by_xpath(f"//tr[{index}]/td[2]").text #raw name from td
@@ -222,144 +245,39 @@ class ScraperSpider(CrawlSpider):
         list_pdf=[pdf_name.split('-application.pdf')[0] for pdf_name in list_pdf] #pdf without pdf ended
         return list_pdf
         
-    def extractPdfDataToCsv(self,pdf_path,csv_path):
-        file_list=glob.glob(pdf_path+"/*.pdf")
-        file_list=[file_path.replace('pdf\\','pdf/') for file_path in file_list]
+    def write_csv(self,element_id,csv_path):
+        print(element_id)
+        print(csv_path)
 
-        #in case at least one pdf exists, the csv timestamp name will be created 
-        if(file_list):
-            file_csv_name=datetime.now().strftime('nursefly-%Y-%m-%d-%H-%M-%S.csv')
-            with open(csv_path+file_csv_name, "w+") as file_output:
-                csv_output = csv.writer(file_output)
-                csv_output.writerow(["Name", "Phone","Email","Address","SSN","Date of Birth","Speciality","Travel Experience"])
+        with open( csv_path +"data" + ".csv", "w") as csvFile:
+            csv_output = csv.writer(csvFile)
+            csv_output.writerow([element_id])
 
-        #the csv listed ordered by datetime of creation    
-        csv_list=glob.glob(csv_path+"/*.csv")
-        csv_list=[file_path.replace('data\\','data/') for file_path in csv_list]
-        self.logger.error(csv_list)
-        for file_path in file_list:
-            raw = parser.from_file(file_path)
-            #focused on content
-            raw = str(raw['content'])
-            raw_lines=raw.splitlines()
+    def reader(self,csv_path):
+        print(csv_path)
+        final_id=None
+        with open(csv_path+"data.csv") as csvFile:
+            csv_output = csv.reader(csvFile)
+            for row in csv_output:
+                data = csv_output
 
-            #here the data will be filter avoiding empty strings
-            data= list(filter(None,raw_lines))
-            #get personal data
-            #get personal data
-            name=data[0]
-            if("PHONE" in data):
-                phone_index=data.index("PHONE")+1
-                phone=data[phone_index]
-            else:
-                phone="Phone not Avaible"
+                if(row is not None and   not (row==[])):
+                    print("El Contenido del CSV es :")
+                    print(row)
+                    final_id=row[0]
+        print("El valor del ID final es : " + final_id)
+        return final_id
 
-            if("EMAIL" in data):
-                email_index=data.index("EMAIL")+1
-                email=data[email_index]
-            else:
-                email="Email not Avaible"
+    def file_exist(self,csv_path):
+        file = pathlib.Path( csv_path +"data.csv")
 
-            if("ADDRESS" in data):
-                address_start_index=data.index("ADDRESS")+1
-                address_end_index=data.index("SSN")
-                address=" ".join(data[address_start_index:address_end_index])
-            else:
-                address="Address not Avaible"
+        if file.exists():
+            print("File exist")
+            return True
+        else:
+            print("File not exist")
+            return False
 
-            if("SSN" in data):
-                ssn_index= data.index("SSN")+1
-                ssn=data[ssn_index]
-                if(ssn == "DATE OF BIRTH"):
-                    ssn="SSN not Avaible"
-            else:
-                ssn="SSN not Avaible"
-                
-            if("DATE OF BIRTH" in data):
-                birth_index=data.index("DATE OF BIRTH")+1
-                birth=data[birth_index]
-                if(birth == "HIGHLIGHTS"):
-                    birth="Date not Avaible"
-            else:
-                birth="Date not Avaible"
-
-            if("SPECIALTY" in data):
-                speciality_start_index=data.index("SPECIALTY")+1
-
-                if("TRAVEL EXPERIENCE" in data):
-                    speciality_end_index=data.index("TRAVEL EXPERIENCE")
-                elif("LICENSES" in data):
-                    speciality_end_index=data.index("LICENSES")
-                
-
-                
-                speciality=" ".join(data[speciality_start_index:speciality_end_index])
-            else:
-                speciality="Specialty not Avaible"
-            
-            if("TRAVEL EXPERIENCE" in data):
-                travel_experience=True
-            else:
-                travel_experience=False
-            name=data[0]
-            if("PHONE" in data):
-                phone_index=data.index("PHONE")+1
-                phone=data[phone_index]
-            else:
-                phone="Phone not Avaible"
-
-            if("EMAIL" in data):
-                email_index=data.index("EMAIL")+1
-                email=data[email_index]
-            else:
-                email="Email not Avaible"
-
-            if("ADDRESS" in data):
-                address_start_index=data.index("ADDRESS")+1
-                address_end_index=data.index("SSN")
-                address=" ".join(data[address_start_index:address_end_index])
-            else:
-                address="Address not Avaible"
-
-            if("SSN" in data):
-                ssn_index= data.index("SSN")+1
-                ssn=data[ssn_index]
-                if(ssn == "DATE OF BIRTH"):
-                    ssn="SSN not Avaible"
-            else:
-                ssn="SSN not Avaible"
-                
-            if("DATE OF BIRTH" in data):
-                birth_index=data.index("DATE OF BIRTH")+1
-                birth=data[birth_index]
-                if(birth == "HIGHLIGHTS"):
-                    birth="Date not Avaible"
-            else:
-                birth="Date not Avaible"
-
-            if("SPECIALTY" in data):
-                speciality_start_index=data.index("SPECIALTY")+1
-
-                if("TRAVEL EXPERIENCE" in data):
-                    speciality_end_index=data.index("TRAVEL EXPERIENCE")
-                elif("LICENSES" in data):
-                    speciality_end_index=data.index("LICENSES")
-                
-
-                
-                speciality=" ".join(data[speciality_start_index:speciality_end_index])
-            else:
-                speciality="Specialty not Avaible"
-            
-            if("TRAVEL EXPERIENCE" in data):
-                travel_experience=True
-            else:
-                travel_experience=False           
-
-
-            with open(csv_list[-1], "a") as file_output:
-                csv_output = csv.writer(file_output)
-                csv_output.writerow([name, phone,email,address,ssn,birth,speciality,travel_experience])
                 
 
 
